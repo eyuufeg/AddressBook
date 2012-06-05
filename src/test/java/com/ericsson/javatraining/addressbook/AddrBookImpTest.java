e0c9bdef7be98a5a1484a96498b7427ca52eac81
Fish.Yu@ericsson.com <eyuufeg@EV101F74FED5A5.eapac.ericsson.se>
2012-06-05 13:46:34 +0800

    change test case package name

---
+++ src/test/java/com/ericsson/javatraining/addressbook/AddrBookImpTest.java
@@ -0,0 +1,38 @@
+package com.ericsson.javatraining.addressbook;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.ericsson.javatraining.addressbook.AddrBookImp;
+import com.ericsson.javatraining.addressbook.ReadAndWrite;
+
+public class AddrBookImpTest {
+    String name = "EMA";
+    String number = "number";
+    String address = "address";
+    ReadAndWrite book;
+    /**
+     * 
+     */
+    AddrBookImp addrbookimp;
+    @Before
+    public void setUp() throws Exception {
+        book = new ReadAndWrite();
+        addrbookimp = new AddrBookImp();
+    }
+
+
+    @Test
+    public void testlist() {
+
+        addrbookimp.find(number, book);
+        addrbookimp.list(book);
+    }
+
+    @Test
+    public void testconvert() {
+        assertEquals("first letter of return value should be upper case", "Ema", addrbookimp.convert(name));
+    }
+}
---
+++ src/test/java/com/ericsson/javatraining/addressbook/ReadAndWriteTest.java
@@ -0,0 +1,50 @@
+package com.ericsson.javatraining.addressbook;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.ericsson.javatraining.addressbook.PhoneModule;
+import com.ericsson.javatraining.addressbook.ReadAndWrite;
+
+public class ReadAndWriteTest {
+
+    ReadAndWrite readandwrite;
+    @Before
+    public void setUp() throws Exception {
+        readandwrite = new ReadAndWrite();
+    }
+
+    @Test
+    public void testAddAndGetphone() {
+
+        PhoneModule phone = new PhoneModule("ema::123::shanghai");
+        readandwrite.addPhone(phone);
+        readandwrite.addtempPhone(phone);
+        assertEquals(phone, readandwrite.getPhone("ema"));
+        assertEquals(phone, readandwrite.gettempPhone("ema"));
+    }
+
+    @Test
+    public void teststore() {
+        try {
+            readandwrite.store();
+        } catch (IOException e) {
+            System.out.println("IOException");
+        }
+    }
+
+    @Test
+    public void testread() {
+        try {
+            readandwrite.read();
+        } catch (IOException e) {
+            System.out.println("IOException");
+        }
+
+
+    }
+}

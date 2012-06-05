4d707df1bce9a9ab7b085d745f6d032d2ed54e85
Fish.Yu@ericsson.com <eyuufeg@EV101F74FED5A5.eapac.ericsson.se>
2012-06-05 13:26:15 +0800

    remove mock

--- src/test/java/com/ericsson/javatraining/addressbooktest/AddrBookImpTest.java
+++ src/test/java/com/ericsson/javatraining/addressbooktest/AddrBookImpTest.java
@@ -1,41 +1,35 @@
 package com.ericsson.javatraining.addressbooktest;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
-import org.slf4j.LoggerFactory;
 
 import com.ericsson.javatraining.addressbook.AddrBookImp;
 import com.ericsson.javatraining.addressbook.PhoneModule;
 import com.ericsson.javatraining.addressbook.ReadAndWrite;
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({ AddrBookImp.class, ReadAndWrite.class, LoggerFactory.class })
+
 public class AddrBookImpTest {
     String name = "EMA";
     String number = "number";
     String address = "address";
     PhoneModule phone = new PhoneModule(name + "::" + number + "::" + address);
-    ReadAndWrite readandwritemock;
+    ReadAndWrite book;
     /**
      * 
      */
     AddrBookImp addrbookimp;
     @Before
     public void setUp() throws Exception {
-        readandwritemock = mock(ReadAndWrite.class);
-        // when(readandwritemock.addPhone(phone)).thenReturn(true);
+        book = new ReadAndWrite();
+
         addrbookimp = new AddrBookImp();
     }
 
 
     @Test
     public void testlist() {
-        ReadAndWrite book = new ReadAndWrite();
+
         addrbookimp.find(number, book);
         addrbookimp.list(book);
     }

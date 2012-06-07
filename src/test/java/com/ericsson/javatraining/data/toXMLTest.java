c11a9b8cd510aa6bbe5bced90fb9f90773f9fae0
Fish.Yu@ericsson.com <eyuufeg@EV101F74FED5A5.eapac.ericsson.se>
2012-06-07 16:00:12 +0800

    update

--- src/test/java/com/ericsson/javatraining/data/toXMLTest.java
+++ src/test/java/com/ericsson/javatraining/data/toXMLTest.java
@@ -2,24 +2,15 @@ package com.ericsson.javatraining.data;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.File;
-
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({ toXMLTest.class, File.class })
 public class toXMLTest {
     private toXML toxml;
     private Document document;
     private Element root;
-    private String FILENAME = "test.xml";
-    private File file;
     @Before
     public void setUp() throws Exception {
         toxml = new toXML();
@@ -33,6 +24,4 @@ public class toXMLTest {
         String name = root.getTagName();
         assertEquals("return value should be addressbook", "addressbook", name);
     }
-
-
 }

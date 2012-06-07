33da80c09a738c5854aaab45a6a68d4a8f08f889
Fish.Yu@ericsson.com <eyuufeg@EV101F74FED5A5.eapac.ericsson.se>
2012-06-07 15:50:04 +0800

    update

--- src/test/java/com/ericsson/javatraining/data/toXMLTest.java
+++ src/test/java/com/ericsson/javatraining/data/toXMLTest.java
@@ -2,17 +2,28 @@ package com.ericsson.javatraining.data;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ toXMLTest.class, File.class })
 public class toXMLTest {
     private toXML toxml;
     private Document document;
     private Element root;
+    private String FILENAME = "test.xml";
+    private File file;
     @Before
     public void setUp() throws Exception {
         toxml = new toXML();
+
     }
 
     @Test
@@ -22,5 +33,6 @@ public class toXMLTest {
         String name = root.getTagName();
         assertEquals("return value should be addressbook", "addressbook", name);
     }
+
 
 }
--- src/test/java/com/ericsson/javatraining/implement/CommandProcessTest.java
+++ src/test/java/com/ericsson/javatraining/implement/CommandProcessTest.java
@@ -1,11 +1,8 @@
 package com.ericsson.javatraining.implement;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.verifyStatic;
-
-import java.io.File;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,10 +11,9 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ CommandProcessTest.class, File.class, Input.class })
+@PrepareForTest({ CommandProcessTest.class, Input.class })
 public class CommandProcessTest {
     private CommandProcess commandprocess;
-    private File mockFile = mock(File.class);
     @Before
     public void setUp() throws Exception {
         commandprocess = new CommandProcess();
@@ -26,18 +22,10 @@ public class CommandProcessTest {
     }
 
     @Test
-    public void testfileexists() {
-        when(mockFile.exists()).thenReturn(false);
+    public void testCommandprocess() {
         commandprocess.process();
         verifyStatic();
         Input.getString("Please input your command: ");
     }
 
-    @Test
-    public void testnofileexists() {
-        when(mockFile.exists()).thenReturn(true);
-        commandprocess.process();
-        verifyStatic();
-        Input.getString("Please input your command: ");
-    }
 }

eb8bb0b4fc02efb6d37706c29bdb483917db304c
Fish.Yu@ericsson.com <eyuufeg@EV101F74FED5A5.eapac.ericsson.se>
2012-06-07 11:20:42 +0800

    add test case

--- src/test/java/com/ericsson/javatraining/implement/CommandProcessTest.java
+++ src/test/java/com/ericsson/javatraining/implement/CommandProcessTest.java
@@ -1,19 +1,23 @@
 package com.ericsson.javatraining.implement;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.verifyStatic;
+
+import java.io.File;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({ CommandProcessTest.class, Input.class })
 public class CommandProcessTest {
     private CommandProcess commandprocess;
-
+    private File mockFile = mock(File.class);
     @Before
     public void setUp() throws Exception {
         commandprocess = new CommandProcess();
@@ -22,10 +26,18 @@ public class CommandProcessTest {
     }
 
     @Test
-    public void test() {
+    public void testfileexists() {
+        when(mockFile.exists()).thenReturn(false);
         commandprocess.process();
         verifyStatic();
         Input.getString("Please input your command: ");
     }
 
+    @Test
+    public void testnofileexists() {
+        when(mockFile.exists()).thenReturn(true);
+        commandprocess.process();
+        verifyStatic();
+        Input.getString("Please input your command: ");
+    }
 }
